8197e98b7835e2e2ea822cccf8bec1b891b59b3c
newxas <ismax.026@gmail.com>
2024-03-13 19:21:57 -0700

    Corrección

--- bulma-collapsible-master/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	globalSetup: './__jest__/setup.js',
-	globalTeardown: './__jest__/teardown.js',
-	testEnvironment: './__jest__/puppeteer_environment.js',
-	// transform: {
-	// 	'^.+\\.jsx$': 'babel-jest'
-	// }
-};
